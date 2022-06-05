9835a81d2efb1154a97cdc25e4d6fad53933cedb
Mitsuru Hashimoto <exhikkii@gmail.com>
2022-06-05 18:06:45 +0900

    24週目のドリル作成

---
+++ week24-1/README.md
@@ -0,0 +1,14 @@
+# 24週目ミニドリル 1問目
+
+## 問題
+
+docker compose upするとエラーが発生します
+
+![picture 8](./images/bcda5a0071f2bbf73eb11dd5404fffa2c2a8914a90d1d9761607897b7ec972fb.png)  
+
+エラー解消するためにはindex.phpの3行目でhost指定をする必要があります。
+適切なhostを指定してください。
+
+### 終了条件
+hostを指定し、画面に接続成功と表示されること
+
---
+++ week24-2/src/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$host = "db";
+$dbname = "posse";
+$user = "root";
+$password = "password";
+
+$create_table_sql = <<<SQL
+   ??? IF NOT EXISTS `students` (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+	name VARCHAR(255) COMMENT '名前',
+	age INT(3) COMMENT '年齢',
+	created_at DATETIME COMMENT '作成日時',
+    updated_at DATETIME COMMENT '更新日時'
+   ) COMMENT='学生';
+SQL;
+
+try {
+    $pdo = new PDO(
+        "mysql:dbname=$dbname;host=$host;charset=utf8mb4",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+    $pdo->query($create_table_sql);
+    echo("テーブル作成成功");
+} catch (PDOException $e) {
+    exit($e->getMessage()); 
+}
