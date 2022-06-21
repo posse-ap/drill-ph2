bd7ea85ef8bddbe0431a89275c96235b6ab952b4
watanabe-miyuki <watanabemiyuki@keio.jp>
2022-06-21 14:13:15 +0900

    week20-3 docker build できない

--- week20-3/index.php
+++ week20-3/index.php
@@ -6,6 +6,8 @@ function round_number( $number ){
 
 $numbers = [1.24,2.32,3.56,4.92,5.02];
 
-$rouned_numbers = array_map(?, ?);
+// $rouned_numbers = array_map($numbers, round());
+
+$rouned_numbers = array_map(function($v){return round($v);}, $numbers);
 
 print_r($rouned_numbers);
