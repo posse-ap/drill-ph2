17f87d94b323f5ea53c5eb27dd7be3ff41cca379
Mitsuru Hashimoto <exhikkii@gmail.com>
2022-07-21 21:22:59 +0900

    28週目の問題作成

---
+++ week28-1/index.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @param {number} num1 - 足したい数字
+ * @param {number} num2 - 足したい数字
+ * @return {number} - 2つの引数を加算した値を返す
+ */
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+
+
+add("string", "string");
