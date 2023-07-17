f9c1b8658154cb120e6cadbf735ff3e922469558
kohei23n <kohei.s.n@gmail.com>
2023-07-17 19:15:11 +0900

    fixed week20

--- src/week20-3-new/index.php
+++ src/week20-3-new/index.php
@@ -1,18 +1,18 @@
 <?php
 
 $host = "db";
-$dbname = "posse";
+$dbname = "ph2drill";
 $user = "root";
 $password = "password";
 
 $create_table_sql = <<<SQL
-   CREATE TABLE IF NOT EXISTS `students` (
+   CREATE TABLE IF NOT EXISTS `people` (
     id INT(11) AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
 	name VARCHAR(255) COMMENT '名前',
 	age INT(3) COMMENT '年齢',
 	created_at DATETIME COMMENT '作成日時',
     updated_at DATETIME COMMENT '更新日時'
-   ) COMMENT='学生';
+   ) COMMENT='人';
 SQL;
 
 try {
@@ -29,7 +29,7 @@ try {
     echo("テーブル作成成功") . PHP_EOL;
 
     # ???に適切に埋め込んでください
-    $sql = 'insert into students (name, age, created_at, updated_at) values (???)';
+    $sql = 'insert into people (name, age, created_at, updated_at) values (???)';
     $query = $pdo->prepare($sql);
     $query->bindValue(':name', "name", PDO::PARAM_STR);
     $query->bindValue(':age', 20, PDO::PARAM_INT);
